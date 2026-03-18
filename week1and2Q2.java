449621824aef5f3377ed6de3c266ed0d03c8c721
cm4690-arch <cm4690@srmist.edu.in>
2026-03-18 10:34:39 +0530

    Add files via upload

---
+++ week1and2Q2.java
@@ -0,0 +1,25 @@
+//Question 2
+public class week1and2Q2 {
+
+    public static void main(String[] args) {
+
+        // Single Word Variable
+        int salary = 100000;   // Good Programming Practice
+        int employeeSalary = 100000; // Improved meaningful name
+
+        // Total Word Variable
+        int annualSalary = 1000000;   // Good Programming Practice
+        int yearlySalary = 1000000;   // Better descriptive name
+
+        // Legal Variable Naming
+        int flatNumber = 1;   // Correct variable name
+        int flat_Number = 1;  // Also valid (underscore allowed)
+
+        // Printing values
+        System.out.println("Salary: " + salary);
+        System.out.println("Employee Salary: " + employeeSalary);
+        System.out.println("Annual Salary: " + annualSalary);
+        System.out.println("Yearly Salary: " + yearlySalary);
+        System.out.println("Flat Number: " + flatNumber);
+    }
+}
